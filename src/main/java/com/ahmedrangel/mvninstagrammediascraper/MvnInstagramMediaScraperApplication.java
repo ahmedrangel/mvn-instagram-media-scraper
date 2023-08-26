bb23da851e41f473c7adfa186f1176e45ce37d2c
Ahmed Rangel <ahmedrangel@outlook.com>
2023-08-26 09:18:32 -0500

    Update MvnInstagramMediaScraperApplication.java

--- src/main/java/com/ahmedrangel/mvninstagrammediascraper/MvnInstagramMediaScraperApplication.java
+++ src/main/java/com/ahmedrangel/mvninstagrammediascraper/MvnInstagramMediaScraperApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MvnInstagramMediaScraperApplication {
   public static void main(String[] args) {
-	  SpringApplication.run(MvnInstagramMediaScraperApplication.class, args);
-	  Scraper.scraper();
+    SpringApplication.run(MvnInstagramMediaScraperApplication.class, args);
+    Scraper.scraper();
   }
 }
